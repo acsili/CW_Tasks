9c60c0229c104366a4009711ec80e5574260cae6
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-03-01 21:42:53 +0400

    update

--- Resh/Program.cs
+++ Resh/Program.cs
@@ -9,12 +9,34 @@ using System.Threading.Tasks;
 namespace Resh
 {
     class Program 
-    {
-        
+    { 
+        public static bool comp(int[] a, int[] b)
+        { 
+            int pr = 0;
+            if (a != null && b != null)
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    for (int j = 0; j < b.Length; j++)
+                    {
+                        if (Math.Sqrt(b[j]) == a[i])
+                        {
+                            pr++;
+                            
+                        }
+                    }
+                    if (pr == 0) return false;
+                    pr = 0;
+                }
+            }
+            else { return false; }
+            return true;
+        }
+
         static void Main(string[] args)
         {
 
-            Console.WriteLine(Task_12.Maskify(""));
+            Console.WriteLine(comp(new int[] { 5, 8, 4 }, new int[] { 64, 16, 15 }));
 
 
             Console.ReadKey();  
