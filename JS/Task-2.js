99b245adf11ed21c5314cf3b1517d604601625de
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-03-04 17:56:49 +0400

    js task 2

--- JS/Task-2.js
+++ JS/Task-2.js
@@ -1,7 +1,47 @@
-function nameShuffler(str)
+/*
+Write an algorithm that will identify valid IPv4 addresses in dot-decimal 
+format. IPs should be considered valid if they consist of four octets, 
+with values between 0 and 255, inclusive.
+
+Valid inputs examples:
+Examples of valid inputs:
+1.2.3.4
+123.45.67.89
+Invalid input examples:
+1.2.3
+1.2.3.4.5
+123.456.78.90
+123.045.067.089
+Notes:
+Leading zeros (e.g. 01.02.03.04) are considered invalid
+Inputs are guaranteed to be a single string
+*/
+
+function isNumeric(str) 
 {
-    let arr = str.split(' ');
-    return arr.reverse().join(' ');
+    if (str.replace(/\D/g,'') == str) return true;
+    else return false;
+
 }
 
-console.log(nameShuffler("adsfdf sdfh cvbn"));
+function isValidIP(str) 
+{
+    let arr = str.split('.');
+    if (arr.length != 4) return false;
+    
+    for (let i = 0; i < arr.length; i++)
+    {
+        if (arr[i].length == '') return false;
+        if (arr[i].length > 3) return false;
+        if (!isNumeric(arr[i])) return false;
+        if (Number(arr[i]) > 255 || Number(arr[i]) < 0) return false;
+        if (arr[i].length > 1 && arr[i][0] == '0') return false;
+        if (arr[i].length > 1 && arr[i][0] == ' ') return false;
+        if (arr[i].length > 1 && arr[i][1] == ' ') return false;
+        if (arr[i].length > 1 && arr[i][2] == ' ') return false;
+    }
+    
+    return true;
+}
+   
+console.log(isValidIP("137..156.2"));
