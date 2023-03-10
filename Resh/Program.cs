baffe445fd1a560cf2e8964dc5e024cbd26cc1f8
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-03-10 21:43:43 +0400

    gitignore update

--- Resh/Program.cs
+++ Resh/Program.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,10 @@ namespace Resh
 {
     class Program 
     {
-       
 
         static void Main(string[] args)
         {
-
-            Console.WriteLine(Task_13.DuplicateEncode("sdasDee +"));
+            //Console.WriteLine("Lorem  ipsum  dolor  sit amet,".Length);
 
 
             Console.ReadKey();  
