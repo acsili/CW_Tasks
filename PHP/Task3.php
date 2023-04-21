1e227fcce58b983162856f2f5daa695ca6a4a721
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-04-21 14:20:59 +0400

    Find the missing letter

---
+++ PHP/Task3.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Find the missing letter
+Write a method that takes an array of consecutive (increasing) 
+letters as input and that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly 
+one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+
+['a','b','c','d','f'] -> 'e'
+['O','Q','R','S'] -> 'P'
+(Use the English alphabet with 26 letters!)
+*/
+function find_missing_letter(array $array): string 
+{
+    $str = "";
+    for ($i = ord(reset($array)), $j = 0; $i <= ord(end($array)) && $j < count($array); $i++, $j++)
+    {
+        if (ord($array[$j]) != $i)
+            return $str .= chr(ord($array[$j]) - 1);
+    }
+    return "";
+}
+
+echo find_missing_letter(['a','b','c','d','f']);
