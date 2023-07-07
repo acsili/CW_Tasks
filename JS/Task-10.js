491ac766a2483c3b693ef4d9d03315982da0bab5
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-07-07 19:53:08 +0400

    js task 10 Which are in?

--- JS/Task-10.js
+++ JS/Task-10.js
@@ -1,19 +1,45 @@
-function solution(number)
+
+/*
+Given two arrays of strings a1 and a2 return a sorted array r in lexicographical
+ order of the strings of a1 which are substrings of strings of a2.
+
+Example 1:
+a1 = ["arp", "live", "strong"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns ["arp", "live", "strong"]
+
+Example 2:
+a1 = ["tarp", "mice", "bull"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns []
+
+Notes:
+Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+Beware: In some languages r must be without duplicates.
+
+*/
+
+
+function inArray(array1,array2)
 {
-    let romanNumbers = ("M", "CMXC", "CM", "D", "CDXC", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
-    let arab = (1000, 990, 900, 500, 490, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
-    let result = ""
-    let i = 0
-    while (number > 0 || arab.length == (i - 1)) 
+    let array = [];
+    for (let i = 0; i < array1.length; i++)
     {
-        while ((number - arab[i]) >= 0) 
+        for (let j = 0; j < array2.length; j++)
         {
-            number -= arab[i]
-            result += romanNumbers[i]
+            if (array2[j].includes(array1[i])) 
+            {
+                array.push(array1[i]);
+                break;
+            }
         }
-        i++
     }
-    return result
+    return array.sort();
 }
 
-console.log(solution(111))
+console.log(inArray(["arp", "live", "strong"], ["lively", "alive", "harp", "sharp", "armstrong"]))
