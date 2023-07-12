7b8f94deb010d919f3f9d7fbd737902ea991f4df
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-07-12 14:40:40 +0400

    js task 12 Simple Sentences

--- JS/Task-12.js
+++ JS/Task-12.js
@@ -1,9 +1,49 @@
-function sumDigits(number) 
-{
-    let sNum = Math.abs(number).toString().split('');
-    let sum = Math.abs(number).toString().split('').reduce((partialSum, a) => partialSum + Number(a), 0);
-    return Math.abs(number).toString().split('').reduce((partialSum, a) => partialSum + Number(a), 0);
-} 
+/*
+Implement a function, so it will produce a sentence out of the given parts.
 
-let s = 123;
-console.log(sumDigits(s));
+Array of parts could contain:
+
+words;
+commas in the middle;
+multiple periods at the end.
+Sentence making rules:
+
+there must always be a space between words;
+there must not be a space between a comma and word on the left;
+there must always be one and only one period at the end of a sentence.
+Example:
+
+makeSentence(['hello', ',', 'my', 'dear']) // returns 'hello, my dear.'
+
+*/
+
+
+
+function makeSentence(parts) 
+{
+    let arr = [];
+    for (let i = 0; i < parts.length - 1; i++)
+    {
+        if (parts[i + 1] == ',')
+        {
+            arr.push(parts[i] + parts[i + 1]);
+            i++;
+        }
+        else 
+        {
+            arr.push(parts[i]);
+        }
+    }
+    arr.push(parts[parts.length - 1]);
+
+    while (arr[arr.length - 1] == '.' || arr[arr.length - 1] == ' ')
+    {
+        arr.pop();
+    }
+
+    return arr.join(' ') + '.';
+}
+
+
+
+console.log(makeSentence(['hello', 'world']));
